9fb09cd10d8282e37047e88d8d22bb69d76d989c
Joel Azemar <joel.azemar@gmail.com>
2022-07-11 16:36:52 +0200

    Update attachment_test.rb

--- tests/6.1.6/attachment_test.rb
+++ tests/6.1.6/attachment_test.rb
@@ -31,5 +31,12 @@ module ActiveStorageOverTime
 
       assert_equal(attachment.asset.id, ActiveStorage.verifier.verify(message, purpose: "blob_id"))
     end
+
+    test "Rails 5.x compatibility" do
+      # Rails 5.x keys the blob_id in the URL
+      message = "eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--6c13b469ea9800834de3cef4976b4ef57c9d7211"
+      assert_equal(attachment.asset.id, ActiveStorage.verifier.verify(message, purpose: "blob_id"))
+    end
+
   end
 end
