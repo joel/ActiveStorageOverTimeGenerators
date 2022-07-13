efc24934ff0167991a2c1dc367a7933824c546ae
Joel Azemar <joel.azemar@gmail.com>
2022-07-13 14:54:16 +0200

    Update attachment_test.rb

--- tests/6.1.6/attachment_test.rb
+++ tests/6.1.6/attachment_test.rb
@@ -5,21 +5,27 @@ module ActiveStorageOverTime
 
     include Rails.application.routes.url_helpers
 
-    test "valid" do
-
+    test "that secret_key_base is correctly set" do
+      # That is essential to ensure that the key is correctly generated
       assert_equal(
         Dummy::Application.secret_key_base,
         "your-secret"
       )
+    end
 
+    test "the consistency of the behavior" do
       attachment = Attachment.new
 
-      attachment.asset = Rack::Test::UploadedFile.new(
-        Rails.root.join("../../test/fixtures/blue.png")
-      )
+      # Test if the old, Rails 5.x, behavior is gone
+      assert_no_difference -> { Dir[Rails.root.join("tmp/storage/**/*")].count(&File.method(:file?)) } do
+        attachment.asset = Rack::Test::UploadedFile.new(
+          Rails.root.join("../../test/fixtures/blue.png")
+        )
+      end
 
       assert Attachment.new.valid?
 
+      # In Rails 6.x the asset is create when the record is saved
       assert_changes -> { Dir[Rails.root.join("tmp/storage/**/*")].count(&File.method(:file?)) }, +1 do
         assert_nothing_raised { attachment.save! }
       end
@@ -35,18 +41,25 @@ module ActiveStorageOverTime
 
       signature = path_for_attachment.match(regex)[:signature]
 
+      # We can find the asset in the storage
       assert_equal(attachment.asset.id, ActiveStorage.verifier.verify(signature, purpose: "blob_id"))
 
+      # From the asset we can find the parent record
       assert_equal(
         attachment.id,
         ActiveStorage::Attachment.where(blob: ActiveStorage::Blob.find_signed(signature)).take.record_id
       )
 
+      # That signature was generated with the correct secret key in Rails 5.x
       ref_signature = "eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--5a1ccbebccd2e79b89c8d7dbbfd17ee1f23d209c"
 
+      # We check the data get encoded the same way over and over again
+      # data--digest
+      # The digest rotates every time
       assert_equal(signature.split("--")[0], ref_signature.split("--")[0])
+    end
 
-      # generated_key = "\xEF*\xA7O'\xB7\u0011\u0014h\x82\x89\xE1\xB1Ģ\xEDt\xF24\x90P\\\b\xE8P=a\a\xFAB\xFCnb漿\u0001\xE5\xD2\xF2DMo\x85\x90C|G\u00140\xBDƾ\xAC\n2P\x9F\u0005\f\xD79\xC8\xE1"
+    test "the generate_key stay constant" do
       generated_key = "\xF8bssAA?\xCCc\xB4\xD5$&>\x91;\xB1^\xBB'uH\u0011 \xED\x8Fi\xF7\xEB\xF2²\xA0\x94\xD9\xE7\xCD-\xBE\xDDcq,\x8C\xFC\x89\xDDS\x9C\xDFH\n\x8C\u0003\x98{\xE4#\u001AR\xA0\xF1\xA4s"
 
       assert_equal(
@@ -55,25 +68,19 @@ module ActiveStorageOverTime
       )
     end
 
-    test "Rails 5.x compatibility" do
-      # Rails 5.x keys the blob_id in the URL
-      signature = "eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--5a1ccbebccd2e79b89c8d7dbbfd17ee1f23d209c"
-      # assert_equal(1, ActiveStorage.verifier.verify(signature, purpose: "blob_id"))
-
-      # assert_equal(1, ActiveStorage.verifier.verify(signature, purpose: "blob_id"))
-
-      # assert_equal(
-      #   1,
-      #   ActiveStorage::Attachment.where(blob: ActiveStorage::Blob.find_signed(signature)).take.record_id
-      # )
-    end
-
-    test "compatibility" do
+    test "that the same message verifier always can decode the token" do
       verifier = ActiveSupport::MessageVerifier.new('foo')
 
+      # Token generated with Rails 5.x
       token = "BAhJIgxmb28gYmFyBjoGRVQ=--c96da3d84a4293a81f3a30c71afe1492ec82e9d6"
 
       assert_equal("foo bar", verifier.verify(token))
+    end
+
+    test "Rails 5.x compatibility" do
+      # Rails 5.x keys the blob_id in the URL
+      signature = "eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--5a1ccbebccd2e79b89c8d7dbbfd17ee1f23d209c"
+      assert_equal(1, ActiveStorage.verifier.verify(signature, purpose: "blob_id"))
     end
 
   end
