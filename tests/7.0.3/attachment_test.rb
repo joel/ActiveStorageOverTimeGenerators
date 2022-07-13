dcb7d9018ca9fa4758a449fa46017011d32986b6
Joel Azemar <joel.azemar@gmail.com>
2022-07-13 16:38:14 +0200

    Update attachment_test.rb

--- tests/7.0.3/attachment_test.rb
+++ tests/7.0.3/attachment_test.rb
@@ -63,17 +63,22 @@ module ActiveStorageOverTime
       # assert_equal(signature, ref_signature)
     end
 
-    # test "the generate_key stay constant" do
-    #   generated_key = "\xF8bssAA?\xCCc\xB4\xD5$&>\x91;\xB1^\xBB'uH\u0011 \xED\x8Fi\xF7\xEB\xF2²\xA0\x94\xD9\xE7\xCD-\xBE\xDDcq,\x8C\xFC\x89\xDDS\x9C\xDFH\n\x8C\u0003\x98{\xE4#\u001AR\xA0\xF1\xA4s"
+    test "the generate_key stay constant" do
+      generated_key = "\xF8bssAA?\xCCc\xB4\xD5$&>\x91;\xB1^\xBB'uH\u0011 \xED\x8Fi\xF7\xEB\xF2²\xA0\x94\xD9\xE7\xCD-\xBE\xDDcq,\x8C\xFC\x89\xDDS\x9C\xDFH\n\x8C\u0003\x98{\xE4#\u001AR\xA0\xF1\xA4s"
 
-    #   assert_equal(
-    #     Dummy::Application.key_generator.generate_key("ActiveStorage").force_encoding('UTF-8'),
-    #     generated_key.force_encoding('UTF-8')
-    #   )
-    # end
+      assert_equal(
+        Dummy::Application.key_generator.generate_key("ActiveStorage").force_encoding("ASCII-8BIT"),
+        generated_key.force_encoding("ASCII-8BIT")
+      )
+    end
 
     test "that the same message verifier always can decode the token" do
       verifier = ActiveSupport::MessageVerifier.new('foo')
+
+      assert_equal(
+        verifier.generate("foo bar"),
+        "BAhJIgxmb28gYmFyBjoGRVQ=--c96da3d84a4293a81f3a30c71afe1492ec82e9d6"
+      )
 
       # Token generated with Rails 5.x
       token = "BAhJIgxmb28gYmFyBjoGRVQ=--c96da3d84a4293a81f3a30c71afe1492ec82e9d6"
