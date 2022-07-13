4c04fa9d4a4a55cf4ea6dcd958e72f8e44075262
Joel Azemar <joel.azemar@gmail.com>
2022-07-13 14:27:35 +0200

    Update attachment_test.rb

--- tests/5.2.8/attachment_test.rb
+++ tests/5.2.8/attachment_test.rb
@@ -6,6 +6,12 @@ module ActiveStorageOverTime
     include Rails.application.routes.url_helpers
 
     test "valid" do
+
+      assert_equal(
+        Dummy::Application.secret_key_base,
+        "your-secret"
+      )
+
       attachment = Attachment.new
 
       assert_changes -> { Dir[Rails.root.join("tmp/storage/**/*")].count(&File.method(:file?)) }, +1 do
@@ -19,7 +25,7 @@ module ActiveStorageOverTime
       assert_nothing_raised { attachment.save! }
 
       regex = %r{/rails/active_storage/blobs/(?<signature>\w+--\w+)/(\w+)\.(\w+)\Z}
-      path_for_attachment = polymorphic_path(attachment.asset, only_path: true)
+      path_for_attachment = polymorphic_path(attachment.asset, only_path: true) # attachment.asset.service_url (need the host set!)
 
       assert_match(
         regex,
@@ -28,6 +34,8 @@ module ActiveStorageOverTime
       )
 
       signature = path_for_attachment.match(regex)[:signature]
+
+      assert ActiveStorage.verifier.valid_message?(signature)
 
       assert_equal(attachment.asset.id, ActiveStorage.verifier.verify(signature, purpose: "blob_id"))
 
@@ -39,6 +47,20 @@ module ActiveStorageOverTime
       ref_signature = "eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--5a1ccbebccd2e79b89c8d7dbbfd17ee1f23d209c"
 
       assert_equal(signature.split("--")[0], ref_signature.split("--")[0])
+      # assert_equal(signature, ref_signature)
+
+      verifier = ActiveSupport::MessageVerifier.new('foo')
+
+      token = verifier.generate("foo bar")
+
+      assert_equal("foo bar", verifier.verify(token))
+
+      generated_key = "\xF8bssAA?\xCCc\xB4\xD5$&>\x91;\xB1^\xBB'uH\x11 \xED\x8Fi\xF7\xEB\xF2\xC2\xB2\xA0\x94\xD9\xE7\xCD-\xBE\xDDcq,\x8C\xFC\x89\xDDS\x9C\xDFH\n\x8C\x03\x98{\xE4#\x1AR\xA0\xF1\xA4s"
+
+      assert_equal(
+        Dummy::Application.key_generator.generate_key("ActiveStorage").force_encoding('UTF-8'),
+        generated_key.force_encoding('UTF-8')
+      )
     end
   end
 end
